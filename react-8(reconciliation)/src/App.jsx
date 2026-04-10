3d24ed5cea30a5bdf43fbec7e1f6cdafbd753c7f
pragatiagrawal69 <your.email@example.com>
2026-04-10 10:43:58 +0530

    reconciliation

---
+++ react-8(reconciliation)/src/App.jsx
@@ -0,0 +1,11 @@
+//class based component
+import React from 'react'
+export default class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Hello from class based App</h1>
+            </div>
+        )
+    }
+}
